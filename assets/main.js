4d47b7ef22458f8fcc82c82534cf64b787936c29
Thomas Verschoren <thomasverschoren@premiumplus.io>
2022-06-18 21:16:31 +0200

    Upload

--- assets/main.js
+++ assets/main.js
@@ -5,11 +5,11 @@ client.invoke('resize', { width: '100%', height: '250px' });
 var order = {"number":42,"status":"shipped","price":1138,"product":["Newton,Pismo"]}
 
 client.get('ticket').then(function(ticket) {
-    var name = ticket['ticket'].requester.name;
+    //Filtering the channels and setting a variable for later use.
     var channel = ticket['ticket'].via.channel
     channel = channel == 'native_messaging'? 'messaging' : channel
-console.log(channel)
     if (channel != 'messaging' && channel != 'chat'){
+        //sendMessage only supports the above two channels, so show an error for other channels.
         $('body').html(`
         <div class="c-callout c-callout--info c-callout--recessed">
               <strong class="c-callout__title">Incompatible channel</strong>
@@ -19,15 +19,20 @@ console.log(channel)
     }
     
     $( "#request" ).click(function() {
-        sendMessage(channel,`Hello ${name}, before we can assist you we need your account number.`) 
+        //Example of sending a message with placeholders from the Zendesk Ticket.
+        sendMessage(channel,`Hello ${ticket['ticket'].requester.name}, before we can assist you we need your order number.`) 
     });
 
     $( "#status" ).click(function() {
+        //Example of sending a reply with info from an external API.
         sendMessage(channel,`Your order ${order.number} is currently ${order.status}`) 
     });
 
     $( "#info" ).click(function() {
-        sendMessage(channel,`Your order ${order.number} contains ${(order.product.length)} product and has a total value of ${order.price}$. You ordered: \n- ${order.product.join('\r\n')}.`) 
+        //Example of sending a burst of short messages
+        sendMessage(channel,`Your order ${order.number} contains ${(order.product.length)} products.`) 
+        sendMessage(channel,`The total amount of ${order.price}`)
+        sendMessage(channel,`You ordered the following items: \n- ${order.product.join('\r\n')}.`) 
     });
 });
 
